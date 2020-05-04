46a5f01f40eaa0447cdda4273546bf4180087929
‘penjoe’ <pennockjoe@gmail.com>
2020-05-03 17:07:40 -0700

    added portfolio files to be deployed on heroku server

--- server.js
+++ server.js
@@ -8,17 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
-
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
+app.get('/', (request, response) => {
+  response.status(200).render('index');
 });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
